cefc5f7d7029a2405dfd42ea12b7e4d4d1d68c62
On <szymon.zblewski@gmail.com>
2020-03-07 16:15:12 +0100

    small changes

--- Janusz/Assets/MainMenu.cs
+++ Janusz/Assets/MainMenu.cs
@@ -6,7 +6,8 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        Debug.Log(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void QuitGame()
     {
